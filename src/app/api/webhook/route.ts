df72e94e03956894cb52c5515f8589875cdf06d7
Cao Chi Hai <haicao2805@gmail.com>
2023-12-25 17:58:44 +0700

    fix: deploy

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -61,6 +61,7 @@ export async function POST(req: Request) {
 
     return new NextResponse("Success", { status: 200 });
   } catch (error) {
+    console.log(error);
     return new NextResponse("Something went wrong.", { status: 500 });
   }
 }
