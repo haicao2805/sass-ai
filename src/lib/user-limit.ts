5bdd21570ba805314f312a9d94105dde16eb3f3d
Cao Chi Hai <haicao2805@gmail.com>
2023-12-25 17:52:48 +0700

    fix: deploy

--- src/lib/user-limit.ts
+++ src/lib/user-limit.ts
@@ -4,79 +4,104 @@ import { prismadb } from "./prismadb";
 import { DAY_IN_MS, MAX_FREE_COUNTS } from "@/constants";
 
 export const getUserLimit = async () => {
-  const { userId } = auth();
+  try {
+    const { userId } = auth();
 
-  if (!userId) return null;
+    if (!userId) return null;
 
-  return await prismadb.userLimit.findUnique({
-    where: {
-      userId: userId,
-    },
-  });
+    return await prismadb.userLimit.findUnique({
+      where: {
+        userId: userId,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
 };
 
 export const getUserLimitCount = async () => {
-  const userLimit = await getUserLimit();
+  try {
+    const userLimit = await getUserLimit();
 
-  if (!userLimit) return 0;
+    if (!userLimit) return 0;
 
-  return userLimit.count;
+    return userLimit.count;
+  } catch (error) {
+    console.log(error);
+    return 0;
+  }
 };
 
 export const checkUserLimit = async () => {
-  const userLimit = await getUserLimit();
+  try {
+    const userLimit = await getUserLimit();
 
-  if (!userLimit || userLimit.count < MAX_FREE_COUNTS) {
-    return true;
+    if (!userLimit || userLimit.count < MAX_FREE_COUNTS) {
+      return true;
+    }
+
+    return false;
+  } catch (error) {
+    console.log(error);
+    return false;
   }
-
-  return false;
 };
 
 export const incrementUserLimit = async () => {
-  const { userId } = auth();
+  try {
+    const { userId } = auth();
 
-  if (!userId) return null;
+    if (!userId) return null;
 
-  const userLimit = await getUserLimit();
+    const userLimit = await getUserLimit();
 
-  if (userLimit) {
-    return await prismadb.userLimit.update({
-      where: { userId },
-      data: { count: userLimit.count + 1 },
+    if (userLimit) {
+      return await prismadb.userLimit.update({
+        where: { userId },
+        data: { count: userLimit.count + 1 },
+      });
+    }
+
+    return await prismadb.userLimit.create({
+      data: { userId, count: 1 },
     });
+  } catch (error) {
+    console.log(error);
+    return null;
   }
-
-  return await prismadb.userLimit.create({
-    data: { userId, count: 1 },
-  });
 };
 
 export const checkSubscription = async () => {
-  const { userId } = auth();
+  try {
+    const { userId } = auth();
 
-  if (!userId) {
+    if (!userId) {
+      return false;
+    }
+
+    const userSubscription = await prismadb.userSubscription.findUnique({
+      where: {
+        userId,
+      },
+      select: {
+        stripeCustomerId: true,
+        stripeCurrentPeriodEnd: true,
+        stripePriceId: true,
+        stripeSubscriptionId: true,
+      },
+    });
+
+    if (!userSubscription) return false;
+
+    const isValid =
+      userSubscription.stripePriceId &&
+      userSubscription.stripeCurrentPeriodEnd?.getTime()! + DAY_IN_MS >
+        Date.now();
+
+    return !!isValid;
+  } catch (error) {
+    console.log(error);
     return false;
   }
-
-  const userSubscription = await prismadb.userSubscription.findUnique({
-    where: {
-      userId,
-    },
-    select: {
-      stripeCustomerId: true,
-      stripeCurrentPeriodEnd: true,
-      stripePriceId: true,
-      stripeSubscriptionId: true,
-    },
-  });
-
-  if (!userSubscription) return false;
-
-  const isValid =
-    userSubscription.stripePriceId &&
-    userSubscription.stripeCurrentPeriodEnd?.getTime()! + DAY_IN_MS >
-      Date.now();
-
-  return !!isValid;
 };
